e4fd38e1fbdf6d794e0f07a19ee6a2bb65407b82
Rosa Regalado <rosaregalado101@gmail.com>
2023-01-26 22:15:24 -0800

    created blockchain

---
+++ blockchain.py
@@ -0,0 +1,64 @@
+class Blockchain(object):
+  
+  def __init__ (self):
+    self.chain = []
+    self.current_transactions = []
+    self.new_block(previous_hash=1, proof=100)
+
+
+  def proof_of_work(self, last_proof):
+  """This method is where you the consensus algorithm is implemented."""
+  proof = 0
+  while self.valid_proof(last_proof, proof) is False:
+    proof += 1
+  return proof
+
+  @staticmethod
+  def valid_proof(last_proof, proof):
+    '''This method validates the block'''
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexigest()
+    return guess_hash[:4] == "0000"
+
+
+  def new_block(self, proof, previous_hash=None):
+    # This function creates new blocks and then adds to the existing chain
+    block = {
+      'index': len(self.chain) + 1,
+      'timestamp': time(),
+      'proof': proof,
+      previous_hash: previous_hash or self.hash(self.chain[-1]),
+    }
+    self.current_transactions = []
+    self.chain.append(block)
+    return block
+
+
+  def new_transaction(self):
+    # This function adds a new transaction to already existing transactions
+    '''This will create a new transaction which will be sent to the next block. It will contain
+    three variables including sender, recipient and amount '''
+    self.current_transactions.append(
+      {
+        'sender': sender,
+        'recipient': recipient,
+        'amount': amount,
+      }
+    )
+    return self.last_block['index'] + 1
+
+
+  # We also have a hash function that is used to create the hash for a block.
+  @staticmethod
+  def hash(block):
+    #Used for hashing a block
+    '''The follow code will create a SHA-256 block hash and also ensure that the dictionary is ordered'''
+    block_string = json.dumps(block, sort_keys=True).encode()
+    return hashlib.sha256(block_string).hexdigest()
+
+
+  @property
+  def last_block(self):
+    # Calls and returns the last block of the chain
+    return self.chain[-1]
+
